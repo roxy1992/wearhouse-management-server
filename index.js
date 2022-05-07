edf180f00bf7e88c716e6a74ccf11da565b89d77
roxy1992 <roxybhuiyan1992@gmail.com>
2022-05-07 15:48:45 +0600

    add delete option

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ async function run() {
             res.send(result);
         })
 
+        //delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
     }
     finally {
